9c5a99726c279f565a80542d3ecc266d3a1cf3ec
Luka Racki <luka.racki@ag04.com>
2021-10-06 11:22:56 +0200

    removing worksflows folder

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,10 +6,7 @@ import { Util } from './util';
 import * as fcl from '@onflow/fcl';
 
 const apiUrl = 'http://localhost:8080';
-// TODO: Change 'P256' -> 'secp256k1'
-// const publicKey = '0a335b0aa7b9f359b0be89e0efc12bfd1696270e2329e49d9954f231e230be8ade7fa562afe569903c994da2ede0b9a8f98cb55ead22ea99ff851644fac93f3e';
-// const flowPublicKey = 'f847b8400a335b0aa7b9f359b0be89e0efc12bfd1696270e2329e49d9954f231e230be8ade7fa562afe569903c994da2ede0b9a8f98cb55ead22ea99ff851644fac93f3e03038203e8';
-// const privateKey = '2b974604c0e314ed165bc753e27b33316106355448658f20edf618772538e47d';
+
 const publicKey =
   '8adf5d29ec027b64c1737e2cb1206143328c7792b98eb5a25203da20d34f5fa67848ccad9be5e2bc57ea5df3801a9ced02dd2faaa7a6ae902f18fde0d8aaef8a';
 const flowPublicKey =
